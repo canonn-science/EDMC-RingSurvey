aa6583282169e1cda05267621ca7dee9ff26d1e1
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2023-06-06 17:08:25 +0100

    Added Ring Distances

--- load.py
+++ load.py
@@ -109,8 +109,8 @@ class Body:
         ringdata["Area"] = (math.pi * pow(ringdata["OuterRad"], 2)) - (
             math.pi * pow(ringdata["InnerRad"], 2)
         )
-        ringdata["Density"] = ringdata["MassMT"] / ringdata["Area"] 
-        ringdata["Width"]=ringdata["OuterRad"]-ringdata["InnerRad"]
+        ringdata["Density"] = ringdata["MassMT"] / ringdata["Area"]
+        ringdata["Width"] = ringdata["OuterRad"] - ringdata["InnerRad"]
         if ringdata["Density"] > 0.001:
             ringdata["Visible"] = True
         else:
@@ -127,15 +127,15 @@ class Body:
             self.rings[ring]["Visible"] = True
 
 
-class cycle():
-
+class cycle:
     def __init__(self, list):
         self.values = list
         self.index = 0
+
     @property
     def len(self):
         return len(self.values)
-    
+
     @property
     def current(self):
         return self.values[self.index]
@@ -149,24 +149,22 @@ class cycle():
         self.index -= 1
         self.index = self.index % len(self.values)
         return self.values[self.index]
-    
-    def append(self,value):
+
+    def append(self, value):
         self.values.append(value)
-        #set the index to the most recent value
-        self.index=len(self.values)-1
+        # set the index to the most recent value
+        self.index = len(self.values) - 1
+
 
 class postUrl(threading.Thread):
     def __init__(self, url):
         threading.Thread.__init__(self)
         self.url = url
-       
 
     def run(self):
         logger.debug("emitter.get")
 
-        r = requests.get(
-            self.url
-        )
+        r = requests.get(self.url)
         if not r.status_code == requests.codes.ok:
             headers = r.headers
             contentType = str(headers["content-type"])
@@ -192,8 +190,12 @@ def plugin_start(plugin_dir):
     Load this plugin into EDMC
     """
     this.plugin_dir = plugin_dir
-    this.IMG_PREV = tk.PhotoImage(file=os.path.join(plugin_dir, "icons", "left_arrow.gif"))
-    this.IMG_NEXT = tk.PhotoImage(file=os.path.join(plugin_dir, "icons", "right_arrow.gif"))
+    this.IMG_PREV = tk.PhotoImage(
+        file=os.path.join(plugin_dir, "icons", "left_arrow.gif")
+    )
+    this.IMG_NEXT = tk.PhotoImage(
+        file=os.path.join(plugin_dir, "icons", "right_arrow.gif")
+    )
 
     return "RingSurvey"
 
@@ -206,53 +208,53 @@ def destroy_titles(event=None):
         this.parent.grid_remove()
     this.startup = False
 
+
 def toggle_visible(index):
     logger.debug(f"toggling visible {index}")
 
     if not this.bodies.current.submitted:
         this.bodies.current.toggle_ring(index)
         if this.tkrings_vis[index]["text"] == "Visible":
-            this.tkrings_vis[index]["text"]="Hidden"
+            this.tkrings_vis[index]["text"] = "Hidden"
             this.tkrings_vis[index].config(foreground="grey")
         else:
-            this.tkrings_vis[index]["text"]="Visible"
+            this.tkrings_vis[index]["text"] = "Visible"
             this.tkrings_vis[index].config(foreground="green")
-    
+
 
 def submit_event(event):
     if not this.bodies.current.submitted:
         logger.debug(f"Clicked to Submit")
-        body=this.bodies.current
+        body = this.bodies.current
         logger.debug(body)
         for ring in body.Rings:
             logger.debug(ring)
-            url=f"https://docs.google.com/forms/d/e/1FAIpQLSfnuNxI3FSf9VqgV1qwz4Z0mvwzOB3rV4weL_gtOy9pKlKXPw/formResponse?usp=pp_url"
-            url+=f"&entry.1920445595={quote(this.cmdr)}"
-            url+=f"&entry.593886049={quote(this.system)}"
-            url+=f"&entry.790394514={this.id64}"
-            url+=f"&entry.2122636={quote(body.Name)}"
-            url+=f"&entry.978850958={quote(ring.get('Name'))}"
-            url+=f"&entry.1583423290={ring.get('Visible')}"
-            url+=f"&entry.1103106375={ring.get('RingClass')}"
-            url+=f"&entry.1378831795={ring.get('MassMT')}"
-            url+=f"&entry.1650707130={ring.get('InnerRad')}"
-            url+=f"&entry.963720103={ring.get('OuterRad')}"
-            url+=f"&entry.1767048738={ring.get('Area')}"
-            url+=f"&entry.1516548742={ring.get('Density')}"
-            url+=f"&entry.1235840073={ring.get('Width')}"
+            url = f"https://docs.google.com/forms/d/e/1FAIpQLSfnuNxI3FSf9VqgV1qwz4Z0mvwzOB3rV4weL_gtOy9pKlKXPw/formResponse?usp=pp_url"
+            url += f"&entry.1920445595={quote(this.cmdr)}"
+            url += f"&entry.593886049={quote(this.system)}"
+            url += f"&entry.790394514={this.id64}"
+            url += f"&entry.2122636={quote(body.Name)}"
+            url += f"&entry.978850958={quote(ring.get('Name'))}"
+            url += f"&entry.1583423290={ring.get('Visible')}"
+            url += f"&entry.1103106375={ring.get('RingClass')}"
+            url += f"&entry.1378831795={ring.get('MassMT')}"
+            url += f"&entry.1650707130={ring.get('InnerRad')}"
+            url += f"&entry.963720103={ring.get('OuterRad')}"
+            url += f"&entry.1767048738={ring.get('Area')}"
+            url += f"&entry.1516548742={ring.get('Density')}"
+            url += f"&entry.1235840073={ring.get('Width')}"
             logger.debug(url)
 
             post(url)
-        this.bodies.current.submitted=True
-        this.submit["text"]="Reported"
+        this.bodies.current.submitted = True
+        this.submit["text"] = "Reported"
         this.submit.config(foreground="grey")
-    
 
-    
 
 def next_body(event):
     this.bodies.next()
     create()
+
 
 def prev_body(event):
     this.bodies.next()
@@ -267,39 +269,59 @@ def create():
     if not this.created:
         this.created = True
         this.parent.grid()
-        this.frame.columnconfigure(2, weight=1)
+        this.frame.columnconfigure(3, weight=1)
         this.frame.grid(sticky="EW")
 
-        this.navigation=tk.Frame(this.frame)
-        this.navigation.grid(row=0, column=0, columnspan=2, sticky="W")
+        this.navigation = tk.Frame(this.frame)
+        this.navigation.grid(row=0, column=0, columnspan=3, sticky="WE")
         this.navigation.columnconfigure(3, weight=1)
 
         this.body = tk.Label(this.navigation)
         this.body.grid(row=0, column=1)
-        
-        
-        this.prev = tk.Button(this.navigation, text="Prev", image=this.IMG_PREV, width=14, height=14, borderwidth=0)
-        this.next = tk.Button(this.navigation, text="Next", image=this.IMG_NEXT, width=14, height=14, borderwidth=0)
+
+        this.prev = tk.Button(
+            this.navigation,
+            text="Prev",
+            image=this.IMG_PREV,
+            width=14,
+            height=14,
+            borderwidth=0,
+        )
+        this.next = tk.Button(
+            this.navigation,
+            text="Next",
+            image=this.IMG_NEXT,
+            width=14,
+            height=14,
+            borderwidth=0,
+        )
         this.prev.grid(row=0, column=0, sticky="W")
         this.next.grid(row=0, column=2, sticky="E")
-        this.prev.bind('<Button-1>', prev_body)
-        this.next.bind('<Button-1>',  next_body  )
+        this.prev.bind("<Button-1>", prev_body)
+        this.next.bind("<Button-1>", next_body)
 
         this.tkrings = []
         this.tkrings_vis = []
+        this.tkrings_range = []
 
         for index in range(3):
             this.tkrings.append(tk.Label(this.frame))
             this.tkrings_vis.append(tk.Label(this.frame))
+            this.tkrings_range.append(tk.Label(this.frame))
+
             this.tkrings[index].grid(row=index + 1, column=0, sticky="W")
             this.tkrings_vis[index].grid(row=index + 1, column=1, sticky="W")
+            this.tkrings_range[index].grid(row=index + 1, column=2, sticky="W")
             this.tkrings[index].grid_remove()
             this.tkrings_vis[index].grid_remove()
-            this.tkrings_vis[index].bind("<Button-1>", lambda event, idx=index: toggle_visible(idx))
+            this.tkrings_range[index].grid_remove()
+            this.tkrings_vis[index].bind(
+                "<Button-1>", lambda event, idx=index: toggle_visible(idx)
+            )
 
             this.submit = tk.Button(this.frame, text="Submit", foreground="green")
-            this.submit.bind('<Button-1>', submit_event)
-            this.submit.grid(row=4,column=0, columnspan=2, sticky="WE")
+            this.submit.bind("<Button-1>", submit_event)
+            this.submit.grid(row=4, column=0, columnspan=2, sticky="WE")
 
             # this.dismiss = tk.Button(this.frame, text="Dismiss", foreground="red")
             # this.dismiss.bind('<Button-1>', destroy)
@@ -308,10 +330,11 @@ def create():
             theme.update(this.frame)
             theme.update(this.navigation)
 
-    #Hide the rings we will unhide some or all later
+    # Hide the rings we will unhide some or all later
     for index in range(3):
         this.tkrings[index].grid_remove()
         this.tkrings_vis[index].grid_remove()
+        this.tkrings_range[index].grid_remove()
 
     bodyname = this.bodies.current.Name
     logger.debug(f"Setting BodyName: {bodyname}")
@@ -322,7 +345,8 @@ def create():
         this.tkrings[index]["text"] = ring.get("Name").replace(f"{bodyname} ", "")
         this.tkrings[index].grid(row=index + 1, column=0, sticky="W")
         this.tkrings_vis[index].grid(row=index + 1, column=1, sticky="W")
-        this.submit.bind('<Button-1>', submit_event)
+        this.tkrings_range[index].grid(row=index + 1, column=2, sticky="W")
+        this.submit.bind("<Button-1>", submit_event)
         if ring.get("Visible"):
             this.tkrings_vis[index]["text"] = "Visible"
             this.tkrings_vis[index].config(foreground="green")
@@ -330,12 +354,16 @@ def create():
             this.tkrings_vis[index]["text"] = "Hidden"
             this.tkrings_vis[index].config(foreground="grey")
 
+        i = round(ring.get("InnerRad") / 299792, 1)
+        o = round(ring.get("OuterRad") / 299792, 1)
+        this.tkrings_range[index]["text"] = f"{i:.1f}-{o:.1f}ls"
+
     if this.bodies.current.submitted:
         logger.debug(f"Hiding Submit")
-        this.submit["text"]="Reported"
+        this.submit["text"] = "Reported"
         this.submit.config(foreground="grey")
     else:
-        this.submit["text"]="Send Report"
+        this.submit["text"] = "Send Report"
         this.submit.config(foreground="green")
         logger.debug(f"Showing Submit")
 
@@ -351,16 +379,16 @@ def create():
         # this.parent.grid()
 
 
-
-
 def destroy(event=None):
     logger.info("destroy")
     this.parent.grid_remove()
     this.frame.grid_remove()
-    
+
+
 def hide_submit():
     logger.debug(f"Hiding Submit in event")
     this.submit.grid_remove()
+
 
 def plugin_app(parent):
     """
@@ -382,76 +410,81 @@ def plugin_app(parent):
 
     this.startup = True
     this.created = False
-    this.bodies=cycle([])
-
+    this.bodies = cycle([])
 
     return this.frame
 
 
 def init_test():
-    this.bodies =cycle([])
-    this.cmdr="Test"    
-    this.id64=1234
-    this.bodies.append(Body(
-        {
-            "timestamp": "2023-05-19T18:37:50Z",
-            "event": "Scan",
-            "ScanType": "AutoScan",
-            "BodyName": f"{this.system} A",
-            "BodyID": 1,
-            "StarSystem": this.system,
-            "SystemAddress": 216618994011,
-            "Rings": [
-                {
-                    "Name": f"{this.system} A A Belt",
-                    "RingClass": "eRingClass_Metalic",
-                    "MassMT": 3.7679e10,
-                    "InnerRad": 1.3762e09,
-                    "OuterRad": 2.8425e09,
-                }
-            ],
-        }
-    ))
-    this.bodies.append(Body(
-        {
-            "timestamp": "2023-05-19T18:37:50Z",
-            "event": "Scan",
-            "ScanType": "AutoScan",
-            "BodyName": f"{this.system} 1",
-            "BodyID": 2,
-            "StarSystem": this.system,
-            "SystemAddress": 216618994011,
-            "Rings": [
-                {
-                    "Name": f"{this.system} 1 A Ring",
-                    "RingClass": "eRingClass_Metalic",
-                    "MassMT": 37679000000000.0,
-                    "InnerRad": 37679000000000.0,
-                    "OuterRad": 37679000000000.0,
-                },
-                {
-                    "Name": f"{this.system} 1 B Ring",
-                    "RingClass": "eRingClass_Metalic",
-                    "MassMT": 37679000000000.0,
-                    "InnerRad": 37679000000000.0,
-                    "OuterRad": 37679000000000.0,
-                },
-                {
-                    "Name": f"{this.system} 1 C Ring",
-                    "RingClass": "eRingClass_Metalic",
-                    "MassMT": 37679000000000.0,
-                    "InnerRad": 37679000000000.0,
-                    "OuterRad": 37679000000000.0,
-                },
-            ],
-        }
-    ))
+    this.bodies = cycle([])
+    this.cmdr = "Test"
+    this.id64 = 1234
+    this.bodies.append(
+        Body(
+            {
+                "timestamp": "2023-05-19T18:37:50Z",
+                "event": "Scan",
+                "ScanType": "AutoScan",
+                "BodyName": f"{this.system} A",
+                "BodyID": 1,
+                "StarSystem": this.system,
+                "SystemAddress": 216618994011,
+                "Rings": [
+                    {
+                        "Name": f"{this.system} A A Belt",
+                        "RingClass": "eRingClass_Metalic",
+                        "MassMT": 3.7679e10,
+                        "InnerRad": 1.3762e09,
+                        "OuterRad": 2.8425e09,
+                    }
+                ],
+            }
+        )
+    )
+    this.bodies.append(
+        Body(
+            {
+                "timestamp": "2023-05-19T18:37:50Z",
+                "event": "Scan",
+                "ScanType": "AutoScan",
+                "BodyName": f"{this.system} 1",
+                "BodyID": 2,
+                "StarSystem": this.system,
+                "SystemAddress": 216618994011,
+                "Rings": [
+                    {
+                        "Name": f"{this.system} 1 A Ring",
+                        "RingClass": "eRingClass_Metalic",
+                        "MassMT": 37679000000000.0,
+                        "InnerRad": 37679000000000.0,
+                        "OuterRad": 37679000000000.0,
+                    },
+                    {
+                        "Name": f"{this.system} 1 B Ring",
+                        "RingClass": "eRingClass_Metalic",
+                        "MassMT": 37679000000000.0,
+                        "InnerRad": 37679000000000.0,
+                        "OuterRad": 37679000000000.0,
+                    },
+                    {
+                        "Name": f"{this.system} 1 C Ring",
+                        "RingClass": "eRingClass_Metalic",
+                        "MassMT": 37679000000000.0,
+                        "InnerRad": 37679000000000.0,
+                        "OuterRad": 37679000000000.0,
+                    },
+                ],
+            }
+        )
+    )
+
 
 def has_rings(rings):
     for ring in rings:
         if ring.get("Name").endswith("Ring"):
             return True
     return False
+
 
 def journal_entry(cmdr, is_beta, system, station, entry, state):
     rtest = (
@@ -460,8 +493,8 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
         and "test ring survey" in entry.get("Message")
     )
 
-    hasrings = (entry.get("Rings") and has_rings(entry.get("Rings")))
-    detected = (hasrings and entry.get("event") in ("Scan"))
+    hasrings = entry.get("Rings") and has_rings(entry.get("Rings"))
+    detected = hasrings and entry.get("event") in ("Scan")
 
     if rtest:
         this.system = system
@@ -471,12 +504,11 @@ def journal_entry(cmdr, is_beta, system, station, entry, state):
 
     if detected:
         this.system = entry.get("StarSystem")
-        this.id64= entry.get("SystemAddress")
-        this.cmdr=cmdr
+        this.id64 = entry.get("SystemAddress")
+        this.cmdr = cmdr
         this.bodies.append(Body(entry))
         create()
 
-    if entry.get("event") in ('FSDJump','StartJump','Location'):
-        this.bodies=cycle([])
+    if entry.get("event") in ("FSDJump", "StartJump", "Location"):
+        this.bodies = cycle([])
         destroy()
-
